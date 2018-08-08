4078e92ec8ab402d133b18d4fc6b046537970a10
Fic <563137447@qq.com>
2018-08-09 07:57:43 +0800

    弹出层增强

--- Banner-js-jquery/js/waterfallFlow.js
+++ Banner-js-jquery/js/waterfallFlow.js
@@ -1,3 +1,23 @@
 $(document).ready(function () {
-    $
+    $("#SignInLink").click(function () {
+        var SignIn = $("#SignIn").html();
+        show(SignIn);
+    });
+
+    $("#SignUpLink").click(function () {
+        var SignUp = $("#SignUp").html();
+        show(SignUp);
+    });
+
+    $("#close").click(function () {
+        $("#layout-mask").hide();
+        $("#layout-pop").hide();
+    });
+
+    function show(html) {
+        $("#layout-mask").show();
+        $("#layout-pop").show();
+        $("#layoutConent").html(html);
+    }
+
 })
